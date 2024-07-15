1f60bca14ae3b0f808e5b6a8f87d0006d3b12970
Gwonyi Moon <102517739+M00NPANG@users.noreply.github.com>
2024-07-15 09:59:40 +0900

    "Basic environment setup configuration"

---
+++ Yeoro/src/test/java/YeoroProject/Yeoro/YeoroApplicationTests.java
@@ -0,0 +1,13 @@
+package YeoroProject.Yeoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
