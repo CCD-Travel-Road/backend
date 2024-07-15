d59d92e6555260d5bf26e1b66b32c5ae7b1af848
Gwonyi Moon <102517739+M00NPANG@users.noreply.github.com>
2024-07-15 21:43:09 +0900

    Database connection test code : MongoDBTestRepository.java

---
+++ Yeoro/src/main/java/YeoroProject/Yeoro/Test/MongoDBTestRepository.java
@@ -0,0 +1,7 @@
+package YeoroProject.Yeoro.Test;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface MongoDBTestRepository extends MongoRepository<MongoDBTestModel, String> {
+
+}
