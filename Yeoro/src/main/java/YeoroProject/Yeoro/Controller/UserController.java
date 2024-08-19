1c5e008394f8f3c1c96fca276a54f3f38843a647
Gwonyi Moon <102517739+M00NPANG@users.noreply.github.com>
2024-08-19 11:50:28 +0900

    유저 컨트롤러

---
+++ Yeoro/src/main/java/YeoroProject/Yeoro/Controller/UserController.java
@@ -0,0 +1,26 @@
+package YeoroProject.Yeoro.Controller;
+
+import YeoroProject.Yeoro.Service.UserService;
+import YeoroProject.Yeoro.Model.User;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getAllUser();
+    }
+}
