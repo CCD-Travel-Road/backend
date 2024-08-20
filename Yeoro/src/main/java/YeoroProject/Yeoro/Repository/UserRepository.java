42a1fe6bc28c49db89b8f85dba568f97a511b3e6
Gwonyi Moon <102517739+M00NPANG@users.noreply.github.com>
2024-08-20 15:03:48 +0900

    유저 리파지토리

---
+++ Yeoro/src/main/java/YeoroProject/Yeoro/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package YeoroProject.Yeoro.Repository;
+
+import YeoroProject.Yeoro.Model.User;
+import java.util.Optional;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+}
