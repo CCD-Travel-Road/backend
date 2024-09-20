9c1588edbdf25239e380a241ed7da0f9074b972d
Gwonyi Moon <102517739+M00NPANG@users.noreply.github.com>
2024-09-21 03:04:08 +0900

    List 형태로 수정

--- Yeoro/src/main/java/YeoroProject/Yeoro/Repository/TravelSpotRepository.java
+++ Yeoro/src/main/java/YeoroProject/Yeoro/Repository/TravelSpotRepository.java
@@ -1,10 +1,13 @@
 package YeoroProject.Yeoro.Repository;
 
 import YeoroProject.Yeoro.Model.TravelSpot;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface TravelSpotRepository extends MongoRepository<TravelSpot, String> {
+
+    List<TravelSpot> findByCourseId(String courseId);
 
     Optional<TravelSpot> findByCourseIdAndTravelSpotName(String existingCourseID, String travelSpotName);
 
