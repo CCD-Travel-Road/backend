3b8c8e81d73dd7721d4fa16f259a64ac9b851878
Gwonyi Moon <102517739+M00NPANG@users.noreply.github.com>
2024-09-21 02:02:49 +0900

    회원가입, 로그인 , 이메일 중복 기능에 대한 컨트롤러

--- Yeoro/src/main/java/YeoroProject/Yeoro/Controller/UserController.java
+++ Yeoro/src/main/java/YeoroProject/Yeoro/Controller/UserController.java
@@ -2,14 +2,19 @@ package YeoroProject.Yeoro.Controller;
 
 import YeoroProject.Yeoro.Service.UserService;
 import YeoroProject.Yeoro.Model.User;
+import YeoroProject.Yeoro.dto.LoginRequest;
+import YeoroProject.Yeoro.dto.ResisterRequest;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("api/users")
 public class UserController {
 
     private final UserService userService;
@@ -22,5 +27,27 @@ public class UserController {
     @GetMapping
     public List<User> getUsers() {
         return userService.getAllUser();
+    }
+    /**
+     * 회원가입
+     * @param resisterRequest 저장할 유저 객체
+     * @return  로그인 성공 여부
+     */
+    @PostMapping("/create")
+    public ResponseEntity<String> createUser(@RequestBody ResisterRequest resisterRequest) {
+        String response = userService.addUser(resisterRequest);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> loginUser(@RequestBody LoginRequest loginRequest) {
+        String response = userService.authenticateUser(loginRequest.getEmail(), loginRequest.getPassword());
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/checkEmail")
+    public ResponseEntity<String> checkEmail(String email) {
+        String response = userService.checkUserEmail(email);
+        return ResponseEntity.ok(response);
     }
 }
